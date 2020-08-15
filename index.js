194b837783e616ead32d0508a191c6204b1f0aa8
lukec11 <me@lukec.me>
2020-08-15 10:24:09 -0400

    send messages correctly in ripcord

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const { App } = require('@slack/bolt');
-const irc = require('irc');
+require("dotenv").config();
+const { App } = require("@slack/bolt");
+const irc = require("irc");
 
 const {
 	IRC_USERNAME,
@@ -13,11 +13,11 @@ const {
 } = process.env;
 
 //sleep 5 seconds before starting app
-;(async () => await new Promise(resolve => setTimeout(resolve, 5000)))();
+(async () => await new Promise(resolve => setTimeout(resolve, 5000)))();
 
 //register irc client
 const client = new irc.Client(IRC_ADDRESS, IRC_USERNAME, {
-	channels: [IRC_BRIDGE_CHANNEL],
+	channels: [IRC_BRIDGE_CHANNEL]
 });
 
 const app = new App({
@@ -26,53 +26,79 @@ const app = new App({
 });
 
 //utility functions
-const getSlackUsername = async (uid) => {
+const getSlackUsername = async uid => {
 	const res = await app.client.users.info({
 		token: process.env.SLACK_BOT_TOKEN,
 		user: uid
 	});
 	return await res.user.profile.display_name_normalized;
-}
+};
 
-const sendToSlackAsUser = async (channel, text, username) => {
-	const res = await app.client.chat.postMessage({
+const slackBlocks = (channel, username, message) => {
+	return {
 		token: process.env.SLACK_BOT_TOKEN,
 		channel: channel,
-		text: text,
 		username: username,
-		icon_emoji: ':speech_balloon:'
-	});
+		icon_emoji: ":speech_balloon:",
+		//use text for the message to Ripcord
+		text: `<${username}> ${message}`,
+		//use blocks for the message to slack
+		blocks: [
+			{
+				type: "rich_text",
+				block_id: "n2N0",
+				elements: [
+					{
+						type: "rich_text_section",
+						elements: [
+							{
+								type: "text",
+								text: message
+							}
+						]
+					}
+				]
+			}
+		]
+	};
+};
+
+const sendToSlackAsUser = async (channel, text, username) => {
+	const res = await app.client.chat.postMessage(slackBlocks(
+channel, username, text)
+	);
 	return await res;
-}
+};
 
 const sendToIrcAsUser = (channel, text, username) => {
 	client.say(channel, `<${username}> ${text}`);
-}
-
+};
 
 //listens for messages from irc
-client.addListener(`message${process.env.IRC_BRIDGE_CHANNEL}`, async (from, message) => {
-	if (from === IRC_USERNAME) {
-		// doesn't send its own messages
-		return;
+client.addListener(
+	`message${process.env.IRC_BRIDGE_CHANNEL}`,
+	async (from, message) => {
+		if (from === IRC_USERNAME) {
+			// doesn't send its own messages
+			return;
+		}
+		await sendToSlackAsUser(SLACK_BRIDGE_CHANNEL, message, from);
 	}
-	await sendToSlackAsUser(SLACK_BRIDGE_CHANNEL, message, from);
-});
+);
 
 client.addListener(`pm`, (from, message) => {
-	console.log('recieved a PM!');
+	console.log("recieved a PM!");
 	console.log(`${from}: ${message}`);
 });
 
-client.addListener('registered', () => {
-	client.say('nickserv', `IDENTIFY ${IRC_PASSWORD}`)
-	console.log('identified');
-	client.send('MODE', IRC_USERNAME, '+B')
-	console.log('set +B');
+client.addListener("registered", () => {
+	client.say("nickserv", `IDENTIFY ${IRC_PASSWORD}`);
+	console.log("identified");
+	client.send("MODE", IRC_USERNAME, "+B");
+	console.log("set +B");
 	client.join(`${IRC_BRIDGE_CHANNEL} ${IRC_CHANNEL_PASSWORD}`);
-	console.log('joined');
-
-})
+	console.log("joined");
+});
 
 //listens for messages from slack
 app.message(async ({ event }) => {
@@ -80,18 +106,22 @@ app.message(async ({ event }) => {
 		//don't send own messages
 		return;
 	}
-	sendToIrcAsUser(IRC_BRIDGE_CHANNEL, event.text, await getSlackUsername(event.user));
-})
+	sendToIrcAsUser(
+		IRC_BRIDGE_CHANNEL,
+		event.text,
+		await getSlackUsername(event.user)
+	);
+});
 
-
-client.addListener('error', (message) => { //listens for errors on irc socket
+client.addListener("error", message => {
+	//listens for errors on irc socket
 	console.error(message);
 });
 
-app.error((error) => { //listens for errors on slack bolt
+app.error(error => {
+	//listens for errors on slack bolt
 	console.error(error);
 });
-
 
 (async () => {
 	await app.start(3000);
