d8a1d8d74cf19e7d5f307afddb9a4cae616d1563
lukec11 <me@lukec.me>
2020-08-16 15:39:28 -0400

    allow for missing fallback or pretext

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ app.message(async ({ event }) => {
 	if (event.hasOwnProperty('attachments')) {
 		let attachments = event.attachments;
 		for (attachment of attachments) {
-			sentMessage += `\n${attachment.pretext}\n${attachment.fallback}\n`
+			sentMessage += `\n${attachment.pretext || ""}\n${attachment.fallback || atachment.text || ""}\n`
 			if (attachment.hasOwnProperty('title_link')) {
 				sentMessage += `${attachment.title_link}\n`
 			}
