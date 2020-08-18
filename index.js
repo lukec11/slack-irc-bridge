b0ffb051646202c54daaa30e00cf783558ca69ae
lukec11 <me@lukec.me>
2020-08-17 21:39:02 -0400

    add support for /me

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
-const { App } = require('@slack/bolt');
-const irc = require('irc');
-const replaceAsync = require('string-replace-async');
+require("dotenv").config();
+const { App } = require("@slack/bolt");
+const irc = require("irc");
+const replaceAsync = require("string-replace-async");
 
 const {
 	IRC_USERNAME,
@@ -14,11 +14,11 @@ const {
 } = process.env;
 
 //sleep 10 seconds before starting app
-;(async () => await new Promise(resolve => setTimeout(resolve, 10000)))();
+(async () => await new Promise(resolve => setTimeout(resolve, 10000)))();
 
 //register irc client
 const client = new irc.Client(IRC_ADDRESS, IRC_USERNAME, {
-	channels: [IRC_BRIDGE_CHANNEL],
+	channels: [IRC_BRIDGE_CHANNEL]
 });
 
 const app = new App({
@@ -27,13 +27,13 @@ const app = new App({
 });
 
 //utility functions
-const getSlackUsername = async (uid) => {
+const getSlackUsername = async uid => {
 	const res = await app.client.users.info({
 		token: process.env.SLACK_BOT_TOKEN,
 		user: uid
 	});
 	return await res.user.profile.display_name_normalized;
-}
+};
 
 const sendToSlackAsUser = async (channel, text, username) => {
 	const res = await app.client.chat.postMessage({
@@ -41,39 +41,52 @@ const sendToSlackAsUser = async (channel, text, username) => {
 		channel: channel,
 		text: text,
 		username: username,
-		icon_emoji: ':speech_balloon:'
+		icon_emoji: ":speech_balloon:"
 	});
 	return await res;
-}
+};
 
 const sendToIrcAsUser = (channel, text, username) => {
 	client.say(channel, `<${username}> ${text}`);
-}
-
+};
 
 //listens for messages from irc
-client.addListener(`message${process.env.IRC_BRIDGE_CHANNEL}`, async (from, message) => {
+client.addListener(
+	`message${process.env.IRC_BRIDGE_CHANNEL}`,
+	async (from, message) => {
+		if (from === IRC_USERNAME) {
+			// doesn't send its own messages
+			return;
+		}
+		await sendToSlackAsUser(SLACK_BRIDGE_CHANNEL, message, from);
+	}
+);
+
+//listens for /me from irc
+client.addListener("action", async (from, message) => {
 	if (from === IRC_USERNAME) {
-		// doesn't send its own messages
+		//doesn't send own /me (should be impossible, but sure)
 		return;
 	}
-	await sendToSlackAsUser(SLACK_BRIDGE_CHANNEL, message, from);
+	//equivalent to slack's /me - italicized text
+	let responseText = `_${message}_`;
+
+	await sendToSlackAsUser(SLACK_BRIDGE_CHANNEL, responseText, from);
 });
 
 client.addListener(`pm`, (from, message) => {
-	console.log('recieved a PM!');
+	console.log("recieved a PM!");
 	console.log(`${from}: ${message}`);
 });
 
-client.addListener('registered', () => {
-	client.say('nickserv', `IDENTIFY ${IRC_PASSWORD}`)
-	console.log('identified');
-	client.send('MODE', IRC_USERNAME, '+B')
-	console.log('set +B');
+client.addListener("registered", () => {
+	client.say("nickserv", `IDENTIFY ${IRC_PASSWORD}`);
+	console.log("identified");
+	client.send("MODE", IRC_USERNAME, "+B");
+	console.log("set +B");
 	client.join(`${IRC_BRIDGE_CHANNEL} ${IRC_CHANNEL_PASSWORD}`);
-	console.log('joined');
-
-})
+	console.log("joined");
+});
 
 //listens for messages from slack
 app.message(async ({ event }) => {
@@ -84,33 +97,51 @@ app.message(async ({ event }) => {
 
 	let sentMessage = event.text;
 
-	if (event.hasOwnProperty('attachments')) {
+	if (event.hasOwnProperty("attachments")) {
 		let attachments = event.attachments;
 		for (attachment of attachments) {
-			sentMessage += `\n${attachment.pretext || ""}\n${attachment.fallback || atachment.text || ""}\n`
-			if (attachment.hasOwnProperty('title_link')) {
-				sentMessage += `${attachment.title_link}\n`
+			sentMessage += `\n${attachment.pretext || ""}\n${
+				attachment.fallback || atachment.text || ""
+			}\n`;
+			if (attachment.hasOwnProperty("title_link")) {
+				sentMessage += `${attachment.title_link}\n`;
 			}
 		}
 	}
 	//deal with @s in messages
-	sentMessage = await replaceAsync(sentMessage, /<@([A-Z0-9]+?)>/g, async (match, p1) => { return `@${await getSlackUsername(p1)}` })
+	sentMessage = await replaceAsync(
+		sentMessage,
+		/<@([A-Z0-9]+?)>/g,
+		async (match, p1) => {
+			return `@${await getSlackUsername(p1)}`;
+		}
+	);
 
 	//deal with links in messages
-	sentMessage = await replaceAsync(sentMessage, /<(http[s]?)\:\/\/([^>|]*)[|]?([^>]*)>/gi, async (match, p1, p2, p3) => { return `${p3}: ${p1}://${p2}\n` });
-	
-	sendToIrcAsUser(IRC_BRIDGE_CHANNEL, sentMessage, await getSlackUsername(event.user) || event.bot_profile.name);
-})
+	sentMessage = await replaceAsync(
+		sentMessage,
+		/<(http[s]?)\:\/\/([^>|]*)[|]?([^>]*)>/gi,
+		async (match, p1, p2, p3) => {
+			return `${p3}: ${p1}://${p2}\n`;
+		}
+	);
 
+	sendToIrcAsUser(
+		IRC_BRIDGE_CHANNEL,
+		sentMessage,
+		(await getSlackUsername(event.user)) || event.bot_profile.name
+	);
+});
 
-client.addListener('error', (message) => { //listens for errors on irc socket
+client.addListener("error", message => {
+	//listens for errors on irc socket
 	console.error(message);
 });
 
-app.error((error) => { //listens for errors on slack bolt
+app.error(error => {
+	//listens for errors on slack bolt
 	console.error(error);
 });
-
 
 (async () => {
 	await app.start(3000);
